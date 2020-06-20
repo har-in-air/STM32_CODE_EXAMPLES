1e60a0647cf4ab003bedf6de2d5ab2476fda19b2
har-in-air <harinnair@yahoo.com>
2020-06-20 17:14:29 +0530

    add project comment for dsp test install

--- dsp/f407_dsp_test_install/Core/Src/main.c
+++ dsp/f407_dsp_test_install/Core/Src/main.c
@@ -1,21 +1,8 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+// DevEBox STM32F407VGT6 dev board
+// Testing CMSIS DSP package installation and RNG hardware random number generation
+// Use fir example code from my CMSIS DSP install repository
+// ~/STM32Cube/Repository/Packs/ARM/CMSIS/5.6.0/CMSIS/DSP/Examples/ARM/arm_fir_example
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
