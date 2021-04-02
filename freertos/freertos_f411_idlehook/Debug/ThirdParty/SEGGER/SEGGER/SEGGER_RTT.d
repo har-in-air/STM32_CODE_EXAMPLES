a88e6af88df1ee55e5e5b2b058be839a0844ace3
har-in-air <harinnair@yahoo.com>
2021-04-02 17:20:25 +0530

    update idlehook

--- freertos/freertos_f411_idlehook/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- /home/hari/STM32CubeIDE/workspace_1.5.1/freertos_f411_idlehook/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-
-/home/hari/STM32CubeIDE/workspace_1.5.1/freertos_f411_idlehook/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
--- freertos/freertos_f411_idlehook/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.d
+++
@@ -1,8 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.c \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- /home/hari/STM32CubeIDE/workspace_1.5.1/freertos_f411_idlehook/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-
-/home/hari/STM32CubeIDE/workspace_1.5.1/freertos_f411_idlehook/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
